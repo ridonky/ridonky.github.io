2efa95dcb33777bd701a2ad6611da170013d864c
Lauren <lauren.perini@gmail.com>
2022-08-31 14:12:11 -0400

    form clear on load

---
+++ assets/js/clearform.js
@@ -0,0 +1,9 @@
+addEventListener('DOMContentLoaded', (event) => {
+    window.onbeforeunload = () => {
+        for(const form of document.getElementsByTagName('form')) {
+          form.reset();
+        }
+      }
+});
+
+
